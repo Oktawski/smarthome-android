3e8b916803726a4c868f5f9b326ea9a2455cd119
Oktawski <oktawian.perlowski@gmail.com>
2021-02-07 21:15:17 +0100

    Minor changes

--- app/src/main/java/com/example/smarthome/relays/ui/RelaysFragment.java
+++ app/src/main/java/com/example/smarthome/relays/ui/RelaysFragment.java
@@ -2,12 +2,10 @@ package com.example.smarthome.relays.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
